f967146341896860c6bf7c610b151de5f4267964
Yan Wang <grapeot@gmail.com>
2013-12-20 19:01:37 -0500

    add some cleanup code

--- src/visualization.py
+++ src/visualization.py
@@ -10,14 +10,16 @@ if len(sys.argv) != 2:
 fn = sys.argv[1]
 basefn = os.path.basename(fn)
 dirfn = os.path.dirname(fn)
+askfn = os.path.join(dirfn, 'asks_' + basefn)
+bidfn = os.path.join(dirfn, 'bids_' + basefn)
 
 # Preprocess
 os.system('coffee preprocess.coffee ' + fn);
-ask = np.loadtxt(os.path.join(dirfn, 'asks_' + basefn))
+ask = np.loadtxt(askfn)
 idx = ask[:, 0] < np.min(ask[:, 0]) * 1.2
 pp.plot(ask[idx, 0], np.cumsum(ask[:, 1])[idx], 'r')
 
-bid = np.loadtxt(os.path.join(dirfn, 'bids_' + basefn))
+bid = np.loadtxt(bidfn)
 idx = bid[:, 0] > np.max(bid[:, 0]) * 0.8
 pp.plot(bid[idx, 0], np.flipud(np.cumsum(np.flipud(bid[:, 1])))[idx])
 
@@ -26,5 +28,5 @@ pp.ylabel('Amount (BTC)')
 pp.legend(['Ask', 'Bid'])
 pp.show()
 
-os.system('rm asks_' + fn)
-os.system('rm bids_' + fn)
+os.system('rm ' + askfn)
+os.system('rm ' + bidfn)
