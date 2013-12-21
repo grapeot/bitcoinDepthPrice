2634fd86abf562a12e31f64c684c70629d4ee84a
Yan Wang <grapeot@gmail.com>
2013-12-21 15:04:28 -0500

    update the gif stitching script and the example figure

--- src/main.sh
+++ src/main.sh
@@ -1,4 +1,5 @@
 ./crawlPrice.sh > ../dat/price.txt
 coffee preprocessPrice.coffee
 python visualize.py
-convert -delay 200 -loop 0 *.png result.gif
+ls ../dat/*.png | xargs -P 4 -I{} bash -c 'echo {}; convert "{}" -scale 50% "{}"'
+convert -delay 100 -loop 0 ../dat/*.png ../dat/result.gif
